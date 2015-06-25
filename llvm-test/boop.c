0f32ddf6baadeed1a5342bf534bce9b944a7f49b
Mudit Ameta <zeusdeux@gmail.com>
2015-06-25 09:27:49 -0400

    Add llvm-test and readme

---
+++ llvm-test/boop.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+// as per http://llvm.org/docs/GettingStarted.html#example-with-clang
+int main() {
+  printf("Hello world!\n");
+  return 1;
+}
---
+++ llvm-test/boop.s
@@ -0,0 +1,29 @@
+	.file	"boop.bc"
+	.text
+	.globl	main
+	.align	16, 0x90
+	.type	main,@function
+main:                                   # @main
+	.cfi_startproc
+# BB#0:
+	pushq	%rax
+.Ltmp1:
+	.cfi_def_cfa_offset 16
+	movl	$.Lstr, %edi
+	callq	puts
+	movl	$1, %eax
+	popq	%rdx
+	ret
+.Ltmp2:
+	.size	main, .Ltmp2-main
+	.cfi_endproc
+
+	.type	.Lstr,@object           # @str
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.Lstr:
+	.asciz	"Hello world!"
+	.size	.Lstr, 13
+
+
+	.ident	"Ubuntu clang version 3.4-1ubuntu3 (tags/RELEASE_34/final) (based on LLVM 3.4)"
+	.section	".note.GNU-stack","",@progbits
